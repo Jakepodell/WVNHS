d24020bb1963cd4197a1e685a6434377b0d74216
Jakepodell <Jake@jakepodell.com>
2015-01-08 12:24:23 -0500

    remove passwords

--- admin/sendMail.php
+++ admin/sendMail.php
@@ -1,28 +1,54 @@
-<?php
-$con=mysqli_connect("localhost","jakepode_user1","Hello.5253","jakepode_hellodb");
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-$message = mysqli_real_escape_string($con, $_POST['message']);
-$subject = mysqli_real_escape_string($con, $_POST['subject']);
-$body = $_POST['message'];
-
-$result = mysqli_query($con,"SELECT * FROM members");
-
-while($row = mysqli_fetch_array($result)) {
-	$mail =mail($row['Email'], $subject, "Dear ".$row['First'].",\r\n\r\n".$body, 'From: WV NHS');
-}
-
-// Send
-
-if($mail){
-echo "Mail delivered";
-}else{
-echo "Problem encountered";
-}
-
-echo "<script type='text/javascript'>window.location.href = 'http://wvnhs.com/admin/mailForm.php'</script>";
-
-?>
+<?php
+
+// Check connection
+
+if (mysqli_connect_errno()) {
+
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+}
+
+
+
+$message = mysqli_real_escape_string($con, $_POST['message']);
+
+$subject = mysqli_real_escape_string($con, $_POST['subject']);
+
+$body = $_POST['message'];
+
+
+
+$result = mysqli_query($con,"SELECT * FROM members");
+
+
+
+while($row = mysqli_fetch_array($result)) {
+
+	$mail =mail($row['Email'], $subject, "Dear ".$row['First'].",\r\n\r\n".$body, 'From: WV NHS');
+
+}
+
+
+
+// Send
+
+
+
+if($mail){
+
+echo "Mail delivered";
+
+}else{
+
+echo "Problem encountered";
+
+}
+
+
+
+echo "<script type='text/javascript'>window.location.href = 'http://wvnhs.com/admin/mailForm.php'</script>";
+
+
+
+?>
+
