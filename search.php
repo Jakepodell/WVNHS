f88d2f4de47f85c6f5c554a67029d52b99299575
Jakepodell <Jake@jakepodell.com>
2015-01-08 12:36:59 -0500

    remove passwords

--- search.php
+++ search.php
@@ -1,22 +1,41 @@
-<?php
-$con=mysqli_connect("localhost","jakepode_user1","Hello.5253","jakepode_hellodb");
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-// escape variables for security
-$name = mysqli_real_escape_string($con, $_POST['name']);
-
-$result = mysqli_query($con,"SELECT * FROM members
-WHERE First='$name'");
-
-while($row = mysqli_fetch_array($result)) {
-  echo $row['First'] . " " . $row['Last']. " " . $row['Grade']. " " . $row['Position'];
-  echo "<br>";
-}
-
-echo "<a href = 'http://www.wvnhs.com/members/all_members.php'> Go Track </a>";
-
-mysqli_close($con);
+<?php
+
+// Check connection
+
+if (mysqli_connect_errno()) {
+
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+}
+
+
+
+// escape variables for security
+
+$name = mysqli_real_escape_string($con, $_POST['name']);
+
+
+
+$result = mysqli_query($con,"SELECT * FROM members
+
+WHERE First='$name'");
+
+
+
+while($row = mysqli_fetch_array($result)) {
+
+  echo $row['First'] . " " . $row['Last']. " " . $row['Grade']. " " . $row['Position'];
+
+  echo "<br>";
+
+}
+
+
+
+echo "<a href = 'http://www.wvnhs.com/members/all_members.php'> Go Track </a>";
+
+
+
+mysqli_close($con);
+
 ?>
