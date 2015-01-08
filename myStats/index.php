1789aa9a1122045141f13dd559c32a5d6d747b9e
Jakepodell <Jake@jakepodell.com>
2015-01-08 12:34:43 -0500

    remove passwords

--- myStats/index.php
+++ myStats/index.php
@@ -1,55 +1,106 @@
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="../events/headbar.css">
-<link rel="stylesheet" type="text/css" href="../events/eventInfoStyle.css">
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="../events/header.js"></script>
-
-
-<style>
-.headbar{
-margin-top:-4em;
-}
-p{
-margin-bottom:0em;
-}
-</style>
-
-</head>
-<body>
-<div class = "headbar">
-		<div class = "signup" id='signup'>Create Account!</div>
-		<div class = "signup" id='admin'>Admin Login</div>
-		<div class = "signup" id='events'>Events</div>
-		<div class = "signup" id='members'>Members</div>
-		<div class = "signup" id='mystats'>My Stats</div>
-		<div class = "signup" id='forms'>Forms</div>
-		<div class = "signup" id='home'>Home</div>
-	</div>
-	
-<?php //starting tag
-
-$con=mysqli_connect("localhost","jakepode_user1","Hello.5253","jakepode_hellodb");
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-echo "<h1>Sign In</h1>";
-
-mysqli_close($con);
-
-//ending tag ?>
-
-<div class = "announcmentWrapper">
-	<div class = "announcment">
-		<form action="Data.php" method="post">
-		<p>User Identification:<br> <input type="text" name="id"></p>
-		<input type="submit">
-		</form>
-	</div>
-</div>
-
-
-</body>
-</html>
+<html>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="../events/headbar.css">
+
+<link rel="stylesheet" type="text/css" href="../events/eventInfoStyle.css">
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<script type="text/javascript" src="../events/header.js"></script>
+
+
+
+
+
+<style>
+
+.headbar{
+
+margin-top:-4em;
+
+}
+
+p{
+
+margin-bottom:0em;
+
+}
+
+</style>
+
+
+
+</head>
+
+<body>
+
+<div class = "headbar">
+
+		<div class = "signup" id='signup'>Create Account!</div>
+
+		<div class = "signup" id='admin'>Admin Login</div>
+
+		<div class = "signup" id='events'>Events</div>
+
+		<div class = "signup" id='members'>Members</div>
+
+		<div class = "signup" id='mystats'>My Stats</div>
+
+		<div class = "signup" id='forms'>Forms</div>
+
+		<div class = "signup" id='home'>Home</div>
+
+	</div>
+
+	
+
+<?php //starting tag
+
+// Check connection
+
+if (mysqli_connect_errno()) {
+
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+}
+
+
+
+echo "<h1>Sign In</h1>";
+
+
+
+mysqli_close($con);
+
+
+
+//ending tag ?>
+
+
+
+<div class = "announcmentWrapper">
+
+	<div class = "announcment">
+
+		<form action="Data.php" method="post">
+
+		<p>User Identification:<br> <input type="text" name="id"></p>
+
+		<input type="submit">
+
+		</form>
+
+	</div>
+
+</div>
+
+
+
+
+
+</body>
+
+</html>
+
